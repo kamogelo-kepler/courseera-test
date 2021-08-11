32a3d3c3d4738333bd0ff290849ec9bb6403a0f2
kamogelo-kepler <71596424+kamogelo-kepler@users.noreply.github.com>
2021-08-11 03:44:29 +0200

    My First Page.

--- site/index.html/buildAndRun.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-call mvn clean package
-call docker build -t com.mycompany/index.html .
-call docker rm -f index.html
-call docker run -d -p 9080:9080 -p 9443:9443 --name index.html com.mycompany/index.html
--- site/index.html/buildAndRun.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-mvn clean package && docker build -t com.mycompany/index.html .
-docker rm -f index.html || true && docker run -d -p 9080:9080 -p 9443:9443 --name index.html com.mycompany/index.html
